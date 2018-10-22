b2d581916f432805716867c85bd1f900f16f5cfa
Patrick Hogan <phogan110@live.com>
2018-10-22 13:11:39 -0600

    pasted `sentences`

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 $('#keyboard-upper-container').hide();//hides upperCase keyboard by default
 
 $('body').keydown(function(e) {
