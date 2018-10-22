6e03a2a9547a852aaa5c735ddf7e4ed288cb170b
Patrick Hogan <phogan110@live.com>
2018-10-22 13:02:55 -0600

    created `keypress` event for highlighting keys on press

--- scripts.js
+++ scripts.js
@@ -4,10 +4,24 @@ $('body').keydown(function(e) {
     if (e.keyCode == 16) {
         $('#keyboard-lower-container').hide();
         $('#keyboard-upper-container').show();
-    }; $(this).keyup(function() {
+    }; $(this).keyup(function(e) {
         if (e.keyCode == 16) {
             $('#keyboard-lower-container').show();
             $('#keyboard-upper-container').hide();
         }
     });
 });//this shows upperCase keyboard on shiftDown and hides it again on shiftUp
+
+$('body').keypress(function(e) {
+    $('#' + e.which).animate({
+        padding: '+=2px',
+        borderRadius: '+=2px',
+        backgroundColor: '#e3e3e3'
+    }, 10, function() {
+        $(this).animate({
+            padding: '-=2px',
+            borderRadius: '-=2px',
+            backgroundColor: '#f5f5f5'
+        });
+    });
+})
