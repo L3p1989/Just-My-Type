230ee142dbc61fa96318657f70a6c1d787efdfac
Patrick Hogan <phogan110@live.com>
2018-10-22 11:39:25 -0600

    added keydown and keyup event that triggers the upperCase keyboard

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
-$('#keyboard-upper-container').hide();
+$('#keyboard-upper-container').hide();//hides upperCase keyboard by default
+
+$('body').keydown(function(e) {
+    if (e.keyCode == 16) {
+        $('#keyboard-lower-container').hide();
+        $('#keyboard-upper-container').show();
+    }; $(this).keyup(function() {
+        if (e.keyCode == 16) {
+            $('#keyboard-lower-container').show();
+            $('#keyboard-upper-container').hide();
+        }
+    });
+});//this shows upperCase keyboard on shiftDown and hides it again on shiftUp
