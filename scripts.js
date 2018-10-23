978acde2d1083c5fc66fc2223c75bb6db6b0a37e
Patrick Hogan <phogan110@live.com>
2018-10-22 19:01:49 -0600

    created date variables; debug why ends with Uncaught TypeError: Cannot read property '0' of undefined

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 var sentenceCount = 0;
 var keyPress = 0;
+var numberOfWords = 54;
+var numberOfMistakes = 0;
+var startTime = new Date();
+var endTime 
 
 $('#keyboard-upper-container').hide();//hides upperCase keyboard by default
 
@@ -21,7 +25,7 @@ $('body').keypress(function (e) {
         padding: '+=2px',
         borderRadius: '+=2px',
         backgroundColor: '#e3e3e3'
-    }, 10, function () {
+    }, function () {
         $(this).animate({
             padding: '-=2px',
             borderRadius: '-=2px',
@@ -30,22 +34,33 @@ $('body').keypress(function (e) {
     }); if (String.fromCharCode(e.which) == sentences[sentenceCount][keyPress]) {
         keyPress++;
         
-        $('#yellow-block').animate({ left: "+=17.5px"});//animates the yellow highlight
+        $('#yellow-block').animate({ left: "+=17.5px"}, .1);//animates the yellow highlight
 
         $('#target-letter').text(sentences[sentenceCount][keyPress]);//changes text of `#target-letter` as keys are pressed
 
-        $('img').attr('src', 'https://t3.ftcdn.net/jpg/00/88/93/72/240_F_88937261_fZzx2GOTcz0ijLz1tuVvZ3MlEH6sfP4B.jpg')
+        $('img').attr('src', 'https://t3.ftcdn.net/jpg/00/88/93/72/240_F_88937261_fZzx2GOTcz0ijLz1tuVvZ3MlEH6sfP4B.jpg');//posts check img to `#feedback`
     } else {
-        $('img').attr('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUrCET8arubXWUHRwAT4mg5vj6ZylmC3_ongmidqgoP9U2UYYa')
-    }
-    
-    
+        numberOfMistakes++;
+        $('img').attr('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUrCET8arubXWUHRwAT4mg5vj6ZylmC3_ongmidqgoP9U2UYYa');//posts red X img to `#feedback`
+    };
     if (sentences[sentenceCount][keyPress] == sentences[0][48] || sentences[1][47] || sentences[2][48] || sentences[3][48])  {
+        
         sentenceCount++;
+        
         keyPress = 0;
-        $('#yellow-block').animate({left: '15px'})
+
+        $('#target-letter').text(sentences[sentenceCount][keyPress]);
+        
+        $('img').attr('src', '');//resets `#feedback` img
+        
+        $('#yellow-block').animate({left: '15px'}, .1);//resets `#yellow-block` position
+        
         $('#sentence').text(sentences[sentenceCount]);//changes text of `#sentence` as the sentences change
-    };//when all conditions are met for a sentence switch to next and reset all values
+    }//when all conditions are met for a sentence switch to next and reset all values
+    else if(sentences[sentenceCount][keyPress] == sentences[4][49]) {
+        var endTime = new Date();
+        var minutes = startTime.getTime() - endTime.getTime();
+    }
 });//this highlights the pressed key for a moment, and tracks the correct key was pressed, as well as moving the yellow cursor accross the screen
 
 $('#sentence').append(sentences[0]);//this appends current sentence to top of the page
